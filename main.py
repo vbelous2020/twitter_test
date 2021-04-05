fe551c37425f6a9855cdd62275fb55e1558e08b7
vbelous2020 <vovabelous2012@gmail.com>
2021-04-05 15:35:00 +0300

    Plan for week TODO

---
+++ main.py
@@ -0,0 +1,10 @@
+# TODO:
+# 1. Create def for main and make one point of start in this main.py file
+# 2. Import all dependency
+# 3. End with media and data in new way(as TweetParser class)
+# 4. Save data into db(from TweetParser class)
+# 5. Remove old way
+# 6. Remove or repair download funcs
+# 7. Make list of 30-40 people to search
+# 8. Calculate time for listening 30-40 twitter users
+# 9. Report
